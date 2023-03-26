70ae0d7b9600ee669fe66ae89ec75617be7c0e65
benmendoza3 <b.mendoza@ufl.edu>
2023-03-26 02:00:43 -0400

    deleted the actual adj close value from each

--- data-csv/zerochecker.py
+++ data-csv/zerochecker.py
@@ -5,18 +5,11 @@ directory = "/Users/bendoza/go/src/WhatIf/data-csv"
 
 # Loop through each file in the directory
 for filename in os.listdir(directory):
-    # Only consider files with the .csv extension
     if filename.endswith(".csv") and filename != "crypto-list.csv":
-        # Open the file for reading
-        with open(os.path.join(directory, filename), "r") as file:
-            # Read the file contents into a list of lines
+        filepath = os.path.join(directory, filename)
+        with open(filepath, "r") as file:
             lines = file.readlines()
-        # Open the file for writing
-        with open(os.path.join(directory, filename), "w") as file:
-            # Loop through each line and write it to the file
-            for i, line in enumerate(lines):
-                # If it's the first line, remove ",Adj Close" from the end
-                if i == 0:
-                    line = line.replace(",Adj Close", "")
-                # Write the line to the file
-                file.write(line)
+        for line_number, line in enumerate(lines):
+            values = line.strip().split(",")
+            if len(values) != 6:
+                print(f"{filename} line {line_number + 1} has {len(values)}")
