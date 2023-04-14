7bff48889c0890ae5e85011e38da8be675a82f63
benmendoza3 <b.mendoza@ufl.edu>
2023-04-14 00:04:41 -0400

    implemented route for worst day to sell portfolio

--- server/routes/GraphPopulate.go
+++ server/routes/GraphPopulate.go
@@ -1,18 +1,18 @@
 package routes
 
 import (
-	"encoding/json"
-	"net/http"
-	"log"
-	"time"
-	"fmt"
-	"strings"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
-	
+
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -34,16 +34,16 @@ func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
 	var TickerValues []string
 
 	if TickerValuesInterface, ok := requestBody["TickerValues"]; ok {
-        if TickerValuesSlice, ok := TickerValuesInterface.([]interface{}); ok {
-            for _, TickerValue := range TickerValuesSlice {
-                if TickerValueString, ok := TickerValue.(string); ok {
-                    TickerValues = append(TickerValues, TickerValueString)
-                }
-            }
-        }
-    }
+		if TickerValuesSlice, ok := TickerValuesInterface.([]interface{}); ok {
+			for _, TickerValue := range TickerValuesSlice {
+				if TickerValueString, ok := TickerValue.(string); ok {
+					TickerValues = append(TickerValues, TickerValueString)
+				}
+			}
+		}
+	}
 
-	TickerValueMap := make(map[string]float64)  
+	TickerValueMap := make(map[string]float64)
 	var Tickers []string
 
 	for _, tv := range TickerValues {
@@ -58,24 +58,23 @@ func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
 
 	TickerString := "('" + strings.Join(Tickers, "', '") + "')"
 
-
 	var BuyDate string = requestBody["BuyDate"].(string)
 	var SellDate string = requestBody["SellDate"].(string)
 
 	buy, err := time.Parse("01/02/2006", BuyDate)
-    if err != nil {
+	if err != nil {
 		log.Fatal(err)
-    }
+	}
 
 	sell, err := time.Parse("01/02/2006", SellDate)
-    if err != nil {
+	if err != nil {
 		log.Fatal(err)
-    }
+	}
 
-    sqlBuyDate := buy.Format("02-JAN-06")
+	sqlBuyDate := buy.Format("02-JAN-06")
 	sqlSellDate := sell.Format("02-JAN-06")
 
-	query := 	`SELECT Ticker, TRUNC(CryptoDate, 'IW') AS WeekStart, AVG(Price) AS WeeklyAverageValue 
+	query := `SELECT Ticker, TRUNC(CryptoDate, 'IW') AS WeekStart, AVG(Price) AS WeeklyAverageValue 
 				FROM DAILYCRYPTOS
 		  		WHERE Ticker IN ` + TickerString + ` 
 		  		AND CryptoDate BETWEEN :startDate AND :endDate 
@@ -84,13 +83,13 @@ func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
 
 	result, err := h.DB.Query(query, sql.Named("startDate", sqlBuyDate), sql.Named("endDate", sqlSellDate))
 	if err != nil {
-   		log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer result.Close()
 
 	// Establishing a response template
 	type GraphDataResponse struct {
-		WeeklyData	map[string]float64	`json: "weeklydata"`
+		WeeklyData map[string]float64 `json: "weeklydata"`
 	}
 
 	var index int = 0
@@ -101,7 +100,7 @@ func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
 		var ticker string
 		var weekStart time.Time
 		var weeklyAvgValue float64
-	
+
 		err := result.Scan(&ticker, &weekStart, &weeklyAvgValue)
 		if err != nil {
 			log.Fatal(err)
@@ -109,21 +108,20 @@ func (h DBRouter) GraphPopulate(w http.ResponseWriter, r *http.Request) {
 
 		portfolioValue += TickerValueMap[ticker] * weeklyAvgValue
 
-
-		if ((index + 1) % 3 == 0) {
+		if (index+1)%len(Tickers) == 0 {
 			weeklyValue[weekStart.Format("01-02-2006")] = portfolioValue
 			portfolioValue = 0
 			fmt.Println(weekStart.Format("01-02-2006"), weeklyValue[weekStart.Format("01-02-2006")])
 		}
 		index++
 	}
-	
+
 	// Setting headers
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	// Setting response map to be the weekly value map created in the loop
-	response := GraphDataResponse { WeeklyData: weeklyValue }
+	response := GraphDataResponse{WeeklyData: weeklyValue}
 
 	// Packing response as type JSON
 	jsonResponse, err := json.Marshal(response)
--- server/routes/WorstSellDay.go
+++ server/routes/WorstSellDay.go
@@ -73,7 +73,12 @@ func (h DBRouter) WorstSellDay(w http.ResponseWriter, r *http.Request) {
 	sqlBuyDate := buy.Format("02-JAN-06")
 	sqlSellDate := sell.Format("02-JAN-06")
 
-	query := ` ` + TickerString + ` `
+	query := `SELECT Ticker, CryptoDate, Price
+			  FROM DAILYCRYPTOS
+	  		  WHERE Ticker IN ` + TickerString + ` 
+	  		  AND CryptoDate BETWEEN :startDate AND :endDate 
+	  		  GROUP BY Ticker, CryptoDate, Price
+	  		  ORDER BY CryptoDate ASC`
 
 	result, err := h.DB.Query(query, sql.Named("startDate", sqlBuyDate), sql.Named("endDate", sqlSellDate))
 	if err != nil {
@@ -81,24 +86,52 @@ func (h DBRouter) WorstSellDay(w http.ResponseWriter, r *http.Request) {
 	}
 	defer result.Close()
 
-	// Establishing a response template
-	type WorstSellResponse struct {
-		Date            string  `json: "date"`
-		PercentIncrease float64 `json: "percentincrease"`
-	}
+	var index int = 0
+	var first bool = true
+	var dailyPortfolioValue float64 = 0
+	dailyValues := make(map[string]float64)
 
-	var ticker string
-	var date time.Time
-	var percentIncrease float64
+	var initialValue float64 = 0
 
-	if result.Next() {
-		err = result.Scan(&ticker, &date, &percentIncrease)
+	for result.Next() {
+		var ticker string
+		var date time.Time
+		var dailyValue float64
+
+		err := result.Scan(&ticker, &date, &dailyValue)
 		if err != nil {
 			log.Fatal(err)
 		}
-	} else {
-		http.Error(w, "No results found", http.StatusNotFound)
-		return
+
+		dailyPortfolioValue += TickerValueMap[ticker] * dailyValue
+
+		if (index+1)%len(Tickers) == 0 {
+			if first {
+				initialValue = dailyPortfolioValue
+				first = false
+			}
+			dailyValues[date.Format("01-02-2006")] = dailyPortfolioValue
+			dailyPortfolioValue = 0
+		}
+		index++
+	}
+
+	var leastDate string
+	var leastValue float64 = 0
+
+	for date, value := range dailyValues {
+		if leastValue == 0 || value < leastValue {
+			leastDate = date
+			leastValue = value
+		}
+	}
+
+	var percentDifference float64 = ((leastValue - initialValue) / initialValue) * 100
+
+	// Establishing a response template
+	type WorstSellResponse struct {
+		Date              string  `json: "date"`
+		PercentDifference float64 `json: "percentdifference"`
 	}
 
 	// Setting headers
@@ -106,7 +139,7 @@ func (h DBRouter) WorstSellDay(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	// Setting response map to be the weekly value map created in the loop
-	response := WorstSellResponse{Date: date.Format("01/02/2006"), PercentIncrease: percentIncrease}
+	response := WorstSellResponse{Date: leastDate, PercentDifference: percentDifference}
 
 	// Packing response as type JSON
 	jsonResponse, err := json.Marshal(response)
